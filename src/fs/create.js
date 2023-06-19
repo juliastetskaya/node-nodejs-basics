73f95321e817804705e3e37ea004011dcedbed29
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2023-06-19 18:01:30 +0300

    feat: implementation of fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import path from 'path';
+import { writeFile } from 'fs/promises';
+
+import { getDirName, isFileExists } from '../utils.js';
+
+
+const __dirname = getDirName(import.meta.url);
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const isExists = await isFileExists(filePath);
+
+        if (isExists) {
+            throw new Error('FS operation failed: file already exists!');
+        }
+
+        await writeFile(filePath, 'I am fresh and young', { flag: 'wx'});
+
+        console.log('File was successfully created!');
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await create();
