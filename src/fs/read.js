d3a450aec53d4aefb51e0a07e2b7a5a5fefa2ba8
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2023-06-19 22:00:48 +0300

    feat: implementation of fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
+import path from 'path';
+import { readFile } from 'fs/promises'; 
+
+import { getDirName, isExists } from '../utils.js';
+
+const __dirname = getDirName(import.meta.url);
+
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const isFileExists = await isExists(filePath);
+
+        if (!isFileExists) {
+            throw new Error('FS operation failed: no file to read!');
+        }
+
+        const content = await readFile(filePath, { encoding: 'utf-8' });
+
+        console.log(content);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await read();
