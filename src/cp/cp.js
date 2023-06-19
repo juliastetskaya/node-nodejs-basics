65fcebbfcd6c93c6cdacde4d424c95ae939dc179
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2023-06-19 23:16:52 +0300

    feat: implementation of cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import path from 'path';
+import { stdin } from 'process';
+import { spawn } from 'child_process';
+import { getDirName } from '../utils.js';
+
+const __dirname = getDirName(import.meta.url);
+
+const filePath = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node', [filePath, ...args]);
+
+    childProcess.stdout.pipe(process.stdout);
+    stdin.pipe(childProcess.stdin);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(process.argv.slice(2));
