345ec8ba037c047189dbfe08d34b7ef2cf52c414
Iuliia Stetskaia <iuliia_stetskaia@epam.com>
2023-06-19 22:47:47 +0300

    feat: implementation of streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import path from 'path';
+import { createReadStream } from 'fs';
+
+import { getDirName } from '../utils.js';
+
+const __dirname = getDirName(import.meta.url);
+
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const stream = createReadStream(filePath, { encoding: 'utf-8' });
+    stream.pipe(process.stdout);
 };
 
 await read();
